957baf59d11c8ac7373f3eadc2767f183cffdc6e
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-12 10:10:06 -0700

    Time: 1 ms (93.79%), Space: 42 MB (95.27%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backTrack(n, 0, 0, new StringBuilder(), res);
+        return res;
+    }
+    
+    public void backTrack(int n, int open, int close, StringBuilder currParen, List<String> res)
+    {
+        if(currParen.length() == 2*n)
+            res.add(currParen.toString());
+        if(open < n)
+        {
+            currParen.append("(");
+            backTrack(n, open + 1, close, currParen, res);
+            currParen.deleteCharAt(currParen.length() - 1);
+        }
+        if(close < open)
+        {
+            currParen.append(")");
+            backTrack(n, open, close + 1, currParen, res);
+            currParen.deleteCharAt(currParen.length() - 1);
+        }
+    }
+}
