2fb16ccf4a81e5073b9168081a34342ab8c205ac
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-13 14:55:54 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (80.50%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,7 +1,10 @@
 class Solution {
+    Map<Integer, Integer> hm = new HashMap<>();
     public int numTrees(int n) {
         if(n <= 1)
             return 1;
+        if(hm.containsKey(n))
+            return hm.get(n);
         int res = 0;
         for(int i = 1; i <= n; i++)
         {
@@ -9,6 +12,7 @@ class Solution {
             int rightHalfCount = numTrees(n - i);
             res += leftHalfCount * rightHalfCount;
         }
+        hm.put(n, res);
         return res;
     }
 }
