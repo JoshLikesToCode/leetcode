f20eee943fd8917ac6217cf63f1a74d39c07f6c5
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-16 18:02:42 -0700

    Time: 7 ms (88.51%), Space: 52 MB (63.17%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.java
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.java
@@ -10,7 +10,7 @@ class Solution {
                 if(grid[i][j] == 1)
                 {
                     StringBuilder sb = new StringBuilder();
-                    bfs(grid, i, j, row, col, sb, "C");
+                    serializeIsland(grid, i, j, row, col, sb, "C");
                     uniqueIslands.add(sb.toString());
                 }
             }
@@ -18,16 +18,16 @@ class Solution {
         return uniqueIslands.size();
     }
     
-    public void bfs(int[][] grid, int i, int j, int r, int c, StringBuilder sb, String dir)
+    public void serializeIsland(int[][] grid, int i, int j, int r, int c, StringBuilder sb, String dir)
     {
         if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == 0)
             return;
         grid[i][j] = 0;
         sb.append(dir);
-        bfs(grid, i + 1, j, r, c, sb, "O");
-        bfs(grid, i - 1, j, r, c, sb, "D");
-        bfs(grid, i, j + 1, r, c, sb, "E");
-        bfs(grid, i, j - 1, r, c, sb, "4");
-        sb.append("LIFE");
+        serializeIsland(grid, i + 1, j, r, c, sb, "O");
+        serializeIsland(grid, i - 1, j, r, c, sb, "D");
+        serializeIsland(grid, i, j + 1, r, c, sb, "E");
+        serializeIsland(grid, i, j - 1, r, c, sb, "U");
+        sb.append("P");
     }
 }
