5d42d13432f51a4400a280b4bdbb4aaf1561017c
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-16 18:00:51 -0700

    Time: 7 ms (88.51%), Space: 52.5 MB (53.47%) - LeetHub

---
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numDistinctIslands(int[][] grid) {
+        Set<String> uniqueIslands = new HashSet<>();
+        int row = grid.length;
+        int col = grid[0].length;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    bfs(grid, i, j, row, col, sb, "K");
+                    uniqueIslands.add(sb.toString());
+                }
+            }
+        }
+        return uniqueIslands.size();
+    }
+    
+    public void bfs(int[][] grid, int i, int j, int r, int c, StringBuilder sb, String dir)
+    {
+        if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == 0)
+            return;
+        grid[i][j] = 0;
+        sb.append(dir);
+        bfs(grid, i + 1, j, r, c, sb, "O");
+        bfs(grid, i - 1, j, r, c, sb, "I");
+        bfs(grid, i, j + 1, r, c, sb, "C");
+        bfs(grid, i, j - 1, r, c, sb, "U");
+        sb.append("E");
+    }
+}
