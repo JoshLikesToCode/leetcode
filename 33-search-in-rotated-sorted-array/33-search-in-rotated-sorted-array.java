de956a04389fd46ddc5ee02e6ca68ab26cc0b847
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-18 12:24:53 -0700

    Time: 1 ms (60.98%), Space: 42.6 MB (44.38%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,49 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length == 1)
-            return (nums[0] == target) ? 0 : -1;
-        int pivotIdx = findPivot(nums, 0, nums.length - 1);
-        if(nums[pivotIdx] == target)
-            return pivotIdx;
-        // search left side
-        if(pivotIdx == 0)
-            return binSearch(nums, 0, nums.length - 1, target);
-        if(nums[0] > target)
-            return binSearch(nums, pivotIdx, nums.length - 1, target);
-        return binSearch(nums, 0, pivotIdx, target);
-    }
-    
-    public int findPivot(int[] nums, int l, int r)
-    {
-        if(nums[l] < nums[r])
-            return 0;
-        while(l <= r)
+        int lo = 0;
+        int hi = nums.length - 1;
+        while(lo <= hi)
         {
-            int mid = l + (r - l)/2;
-            if(nums[mid] > nums[mid+1])
-                return mid + 1;
-            else
-            {
-                if(nums[mid] < nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-        }
-        return 0;
-    }
-    
-    public int binSearch(int[] nums, int l, int r, int target)
-    {
-        while(l <= r)
-        {
-            int mid = l + (r - l)/2;
+            int mid = lo + (hi - lo) / 2;
             if(nums[mid] == target)
                 return mid;
-            if(nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+            if(nums[lo] <= nums[mid]) // left side sorted in ascending order
+            {
+                if(target >= nums[lo] && target < nums[mid])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            }
+            else // right side sorted in ascending order
+            {
+                if(target > nums[mid] && target <= nums[hi])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
         }
         return -1;
     }
