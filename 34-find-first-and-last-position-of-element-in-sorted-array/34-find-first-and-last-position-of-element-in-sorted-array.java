82e235b2a37ae21453d6bbee785b9b9797a91504
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-19 17:40:16 -0700

    Time: 0 ms (100.00%), Space: 47.4 MB (29.95%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[]{-1,-1};
-        res[0] = search(nums, target, false);
-        if(res[0] != -1) // only process if we found the target
-            res[1] = search(nums, target, true);
+        res[0] = modBinSearch(nums, 0, nums.length - 1, target, false);
+        if(res[0] != -1)
+            res[1] = modBinSearch(nums, 0, nums.length - 1, target, true);
         return res;
     }
-    public int search(int[] nums, int key, boolean findMaxIdx)
+    
+    public int modBinSearch(int[] nums, int lo, int hi, int target, boolean findMax)
     {
         int keyIdx = -1;
-        int lo = 0;
-        int hi = nums.length - 1;
         while(lo <= hi)
         {
             int mid = lo + (hi - lo) / 2;
-            if(nums[mid] < key)
+            if(nums[mid] < target)
                 lo = mid + 1;
-            else if(nums[mid] > key)
+            else if(nums[mid] > target)
                 hi = mid - 1;
-            else // nums[mid] == key
+            else // nums[mid] == target
             {
                 keyIdx = mid;
-                if(findMaxIdx)
+                if(findMax)
                     lo = mid + 1;
                 else
                     hi = mid - 1;
