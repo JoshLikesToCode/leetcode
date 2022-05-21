20b4a266eb7e1f6e38431f68de408e9d56cd1d53
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-21 07:52:29 -0700

    Time: 2 ms (63.21%), Space: 43.2 MB (83.19%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        UnionFind UF = new UnionFind(isConnected.length);
+        for(int i = 0; i < isConnected.length; i++)
+        {
+            for(int j = 0; j < isConnected[0].length; j++)
+            {
+                if(isConnected[i][j] == 1)
+                    UF.union(i, j);
+            }
+        }
+        return UF.count();
+    }
+    
+    class UnionFind
+    {
+        int[] parents;
+        public UnionFind(int n)
+        {
+            parents = new int[n];
+            for(int i = 0; i < n; i++)
+                parents[i] = i;
+        }
+        
+        public int find(int vertex)
+        {
+            // path compression
+            if(parents[vertex] == vertex)
+                return vertex;
+            return parents[vertex] = find(parents[vertex]);
+        }
+        
+        public void union(int v1, int v2)
+        {
+            v1 = find(v1);
+            v2 = find(v2);
+            if(v1 != v2)
+                parents[v1] = v2;
+        }
+        
+        public int count()
+        {
+            // get count of connected componenets
+            int count = 0;
+            for(int i = 0; i < parents.length; i++)
+            {
+                if(parents[i] == i)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
