29306e0ccc9db9af228e464ef5e0e2e884364512
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-22 12:46:24 -0700

    Time: 21 ms (6.74%), Space: 48.4 MB (45.26%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        
         Stack<Integer> astStack = new Stack<>();
         for(int a : asteroids)
         {
             boolean blownUp = false;
             int currAst = a;
-            while(!astStack.isEmpty() && astStack.peek() > 0 && currAst < 0) // only asteroids moving in opposite directions will result in a collision
+            while(!astStack.isEmpty()  && astStack.peek() > 0 && currAst < 0)
             {
                 int topAst = astStack.pop();
-                if(topAst == -currAst)  // only asteroids moving in different directions and the same size result in a blow up
+                if(topAst == -currAst)
                 {
                     blownUp = true;
                     break;
                 }
-                else // if the asteroids collide and are not the same size, the asteroid with the largest size remains
+                else
                 {
                     if(topAst > -currAst)
-                       currAst = topAst;
+                        currAst = topAst;
                 }
             }
-            if(!blownUp) // first asteroid goes here, and currAsteroids that passes without an explosion
-                astStack.push(currAst);
+            if(!blownUp)
+                astStack.add(currAst);
         }
+        
         int[] res = new int[astStack.size()];
         int resIdx = 0;
-        for(Integer a : astStack)
-            res[resIdx++] = a;
+        for(int addAst : astStack)
+            res[resIdx++] = addAst;
         return res;
     }
 }
