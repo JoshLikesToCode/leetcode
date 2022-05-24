0e95110c292c13961ab9acecbfbfd9bae754692d
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-05-24 12:46:22 -0700

    Time: 1 ms (16.44%), Space: 41.2 MB (22.39%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,18 +1,20 @@
 class Solution {
-    Map<Integer, Integer> hm = new HashMap<>();
+    // memo
+    Map<Integer, Integer> cachememo = new HashMap<>();
+    
     public int numTrees(int n) {
         if(n <= 1)
             return 1;
-        if(hm.containsKey(n))
-            return hm.get(n);
+        if(cachememo.containsKey(n))
+            return cachememo.get(n);
         int res = 0;
         for(int i = 1; i <= n; i++)
         {
-            int leftHalfCount = numTrees(i - 1);
-            int rightHalfCount = numTrees(n - i);
-            res += leftHalfCount * rightHalfCount;
+            int leftHalf = numTrees(i - 1);
+            int rightHalf = numTrees(n - i);
+            res += leftHalf * rightHalf;
         }
-        hm.put(n, res);
+        cachememo.put(n, res);
         return res;
     }
 }
