8d0b8bf8b8e56af3c7f9654cd2f99eafea599d70
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-06-17 10:33:28 -0700

    Time: 17 ms (33.53%), Space: 47.7 MB (50.78%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int maxVal = 0;
+        for (int n: nums) 
+		{
+            hm.put(n, (hm.getOrDefault(n, 0) + 1));
+            maxVal = Math.max(maxVal, n);
+        }
+        
+        int[] dp = new int[maxVal+1];
+        dp[1] = 1 * hm.getOrDefault(1, 0);
+        
+        for (int i = 2; i <= maxVal; i++) 
+            dp[i] = Math.max(dp[i-1], dp[i-2] + i * hm.getOrDefault(i, 0));
+        return dp[maxVal];
+    }
+}
