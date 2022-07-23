a0d609c851b13ec371f17d9d81a552438a64f757
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-07-22 22:00:46 -0700

    Time: 168 ms (16.28%), Space: 39.2 MB (33.72%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cs
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cs
@@ -1,22 +1,22 @@
 public class Solution {
     public bool UniqueOccurrences(int[] arr) {
-        Dictionary<int, int> hm = new Dictionary<int, int>();
+        Dictionary<int, int> dict = new Dictionary<int, int>();
         foreach(int n in arr)
         {
-            if(!hm.ContainsKey(n))
-                hm.Add(n, 1);
+            if(!dict.ContainsKey(n))
+                dict.Add(n, 1);
             else
-                {
-                    int val = hm[n] + 1;
-                    hm[n] = val;
-                }
+            {
+                int val = dict[n] + 1;
+                dict[n] = val;
+            }
         }
         HashSet<int> hs = new HashSet<int>();
         Array.Sort(arr);
         int prev = -1001;
         for(int i = 0; i < arr.Length; i++)
         {
-            int val = hm[arr[i]];
+            int val = dict[arr[i]];
             if(prev == arr[i])
                 continue;
             prev = arr[i];
