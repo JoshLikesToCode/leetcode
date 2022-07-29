ce8645688e51fe7bcb2a7a7cf940519d3d041c30
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-07-29 14:22:59 -0700

    Time: 204 ms (76.16%), Space: 45.3 MB (72.67%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cs
@@ -0,0 +1,29 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public int MaxDepth(Node root) {
+        if(root == null)
+            return 0;
+        int res = 0;
+        foreach(var node in root.children)
+            res = Math.Max(res, MaxDepth(node));
+        return res + 1;
+    }
+}
