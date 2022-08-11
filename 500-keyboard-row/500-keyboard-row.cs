56331647542307f53f0770f2cc38794a32fa3f52
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-08-11 15:33:51 -0700

    Time: 210 ms (61.36%), Space: 43.5 MB (30.68%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public string[] FindWords(string[] words) {
+        string[] allRows = new string[3];
+        allRows[0] = "QWERTYUIOP";
+        allRows[1] = "ASDFGHJKL";
+        allRows[2] = "ZXCVBNM";
+        Dictionary<char, int> charByRow = new Dictionary<char, int>();
+        for(int i = 0; i < allRows.Length; i++)
+        {
+            foreach(char c in allRows[i].ToCharArray())
+            {
+                charByRow[c] = i;
+                // Console.WriteLine("i = " + i + ", c = " + c + ", and charByRow[c] = " + charByRow[c]);
+            }
+        }
+        List<string> res = new List<string>();
+        foreach(string s in words)
+        {
+            if(s == "")
+                continue;
+            int currRow = -1;
+            bool addOrNot = true;
+            foreach(char c in s.ToUpper().ToCharArray())
+            {
+                if(currRow == -1)
+                {
+                    currRow = charByRow[c];
+                    continue;
+                }
+                if(currRow != charByRow[c])
+                {
+                    addOrNot = false;
+                    break;
+                }
+            }
+            if(addOrNot)
+                res.Add(s);
+        }
+        return res.ToArray();
+    }
+}
