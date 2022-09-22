f851554fac79808a79fa06c2a3b79433e82f4fc4
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-09-22 07:29:09 -0700

    Time: 132 ms (18.05%), Space: 36.6 MB (40.78%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cs
+++ 392-is-subsequence/392-is-subsequence.cs
@@ -2,15 +2,12 @@ public class Solution {
     public bool IsSubsequence(string s, string t) {
         int a = 0;
         int b = 0;
-        char[] sArr = s.ToCharArray();
-        char[] tArr = t.ToCharArray();
-        
         while(a < s.Length && b < t.Length)
         {
-            if(sArr[a] == tArr[b])
+            if(s[a] == t[b])
                 a++;
             b++;
         }
-        return (a==s.Length);
+        return (a == s.Length);
     }
 }
